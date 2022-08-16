cb31ee977616d61e7fd2c11698875487419343ff
Thomas-rnd <renaudthomas@orange.fr>
2022-08-16 19:30:39 +0200

    Create head.php

---
+++ includes/head.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Site crée par 2 élèves de l'ENSC ayant pour but de mettre à disposition des histoires dont vous êtes le héros">
+    <meta name="author" content="LEGER Sébastien et RENAUD Thomas">
+    <meta name="docsearch:language" content="fr">
+
+    <!-- Bootstrap core CSS --> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+    <title>RENAUD LEGER <?php if (isset($pageTitle)) echo "- $pageTitle"; ?></title>
+</head>
