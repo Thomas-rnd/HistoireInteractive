01d7ff44cf44aa5203260c049483dc1249c0b357
Thomas-rnd <renaudthomas@orange.fr>
2022-08-16 19:30:32 +0200

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer-dark bg-dark text-center mb-0">
+  <div class="text-center p-3">
+    <span class="text-white">© 2022 Copyright :</span>
+      <a class="text-white" href="https://github.com/Thomas-rnd/ComWeb_S6" target="_blank">RENAUD_LEGER/ComWeb_S6</a>
+  </div>
+</footer>
